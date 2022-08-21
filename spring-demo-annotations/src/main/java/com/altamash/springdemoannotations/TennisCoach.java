13c6591d821a04b2acbe2e0d21752290ff5e3bd8
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-08-21 10:46:49 +0530

    other injections

--- spring-demo-annotations/src/main/java/com/altamash/springdemoannotations/TennisCoach.java
+++ spring-demo-annotations/src/main/java/com/altamash/springdemoannotations/TennisCoach.java
@@ -7,12 +7,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    // Field Injection
+    @Autowired
     private FortuneService fortuneService;
 
+    // Constructor injection
+    /*
     @Autowired
     TennisCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
+    */
+
+    // Setter Injection
+    /*
+     @Autowired
+    public void setFortuneService(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+     */
+
+     // Method Injection
+    /*
+     @Autowired
+     public void methodInjectionExample(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+     }
+     */
 
     @Override
     public String getDailyWorkout() {
